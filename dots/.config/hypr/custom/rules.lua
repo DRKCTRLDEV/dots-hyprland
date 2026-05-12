4880dce487d8ff689953ef4ebd9a50060c2db4e3
drkctrldev <alfiew09uk@proton.me>
2026-05-12 17:00:37 +0100

    Remove Hyprland .conf files and use Lua rules

--- dots/.config/hypr/custom/rules.lua
+++ dots/.config/hypr/custom/rules.lua
@@ -1,1 +1,24 @@
+-- You can put custom rules here.
+-- Window/layer rules: https://wiki.hyprland.org/Configuring/Window-Rules/
+-- Workspace rules: https://wiki.hyprland.org/Configuring/Workspace-Rules/
 
+-- ##### Preset WindowRules #####
+-- Blur/Transparency Global:
+hl.window_rule({ match = { class = ".*" }, opacity = "0.89 override 0.89 override" })
+hl.window_rule({ match = { class = ".*" }, no_blur = false })
+
+-- Blur/Transparency Whitelist:
+-- hl.window_rule({ match = { class = "(?i).*(dolphin|code|kitty|spotify).*" }, opacity = "0.89 override 0.89 override" })
+-- hl.window_rule({ match = { class = "(?i).*(dolphin|code|kitty|spotify).*" }, no_blur = false })
+
+-- Blur/Transparency Blacklist:
+-- hl.window_rule({ match = { class = "(?i).*(dolphin|code|kitty|spotify).*" }, opacity = "1 override 1 override" })
+-- hl.window_rule({ match = { class = "(?i).*(dolphin|code|kitty|spotify).*" }, no_blur = true })
+
+-- Disable blur for XWayland (compatibility):
+-- hl.window_rule({ match = { xwayland = 1 }, no_blur = true })
+
+-- ##### Custom WindowRules #####
+hl.window_rule({ match = { class = "^(steam)$", title = "^(?!Steam$).*$" }, float = true })
+hl.window_rule({ match = { class = "^(steam)$", title = "^(?!Steam$).*$" }, opacity = "1 override" })
+hl.window_rule({ match = { class = "^(steam)$", title = "^(?!Steam$).*$" }, no_blur = true })

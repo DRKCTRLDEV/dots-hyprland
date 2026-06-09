ed00dce6e4490fc3494e25fea90a753dc28f9c70
DrkCtrlDev <alfiew09uk@proton.me>
2026-06-09 22:24:20 +0100

    force `overstrike.exe` to always float, Fixes

--- dots/.config/hypr/hyprland/rules.lua
+++ dots/.config/hypr/hyprland/rules.lua
@@ -46,7 +46,8 @@ hl.window_rule({match = {title = ".*Shell conflicts.*" },                    flo
 hl.window_rule({match = {class = "org.freedesktop.impl.portal.desktop.kde" }, float = true})
 hl.window_rule({match = {class = "org.freedesktop.impl.portal.desktop.kde" }, size = {"(monitor_w*0.60)", "(monitor_h*0.65)"} })
 hl.window_rule({match = {class = "^(Zotero)$" },                             float = true})
-hl.window_rule({match = {class = "^(Zotero)$" },                             size = {"(monitor_w*0.45)", "(monitor_h*0.45)"} })
+hl.window_rule({ match = { class = "^(Zotero)$" }, size = { "(monitor_w*0.45)", "(monitor_h*0.45)" } })
+hl.window_rule({match = {class = "^(overstrike.exe)$" },                     float = true})
 
 -- Move
 -- kde-material-you-colors spawns a window when changing dark/light theme. This is to make sure it doesn't interfere at all.

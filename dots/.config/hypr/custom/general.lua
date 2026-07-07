381ed9ff647196b9172702fd863407aac1f5ac9f
DrkCtrlDev <AlfieW09UK@proton.me>
2026-07-07 21:33:23 +0100

    Add commented VRR config example

--- dots/.config/hypr/custom/general.lua
+++ dots/.config/hypr/custom/general.lua
@@ -3,6 +3,7 @@
 
 -- Monitor config (auto-detect with preferred settings)
 -- hl.monitor({ output = "DP-1", mode = "3440x1440@165" })
+-- hl.config({ misc = { vrr = 3 } })
 
 -- Input config: GB keyboard layout
 -- hl.config({ input = { kb_layout = "gb" } })
